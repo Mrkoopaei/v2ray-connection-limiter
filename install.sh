f67369dfb69ccb680d8739b1bb4423badc237c9a
MohammadReza Koopaei <mohamad.koopaei@gmail.com>
2023-04-28 10:02:10 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 !#/bin/bash
 
 apt-get update && apt-get upgrade
-
+apt-get install git
 apt-get install python
 apt-get install pip
 pip install --upgrade schedule
 pip install --root-user-action=ignore requests
 pip install psutil
+
+git clone https://github.com/Mrkoopaei/v2ray-connection-limiter.git
 
 chmod +x start.py
 chmod +x main.py
